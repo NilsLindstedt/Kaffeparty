bdbe5434eece8ef5a4cf863cf2a16a2535364590
User <User@DESK-CJ2GJ32>
2017-10-10 14:08:30 +0200

    Add project files.

---
+++ Kaffeparty/Scripts/myJavaScript.js
@@ -0,0 +1,7 @@
+﻿
+myDate();
+
+function myDate() {
+    var d = new Date();
+    document.getElementById("footer").innerText = d.toDateString() + " " + document.getElementById("footer").innerText;
+}
